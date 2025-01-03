5306a4f79a6f9fc58aa3ef837a8589cc04052bd4
Tshuki021 <shubhanshukrsharma@gmail.com>
2025-01-03 20:43:33 +0530

    03-01-2025

---
+++ README.md
@@ -0,0 +1,5 @@
+conda create -p venv python==3.8
+
+conda activate ./venv
+
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Tshuki",
+    author_email="shubhanshukrsharma@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
